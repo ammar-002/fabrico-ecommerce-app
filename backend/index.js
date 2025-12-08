69981bb26668ea32ad3cc854a2743529e682ebfb
Muhammad Ammar <iammuhammadammar2@gmail.com>
2025-12-09 02:02:19 +0500

    comment out app.listen for debugging purposes

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use("/api/v1/cart", cartRouter)
 
 
 
-app.listen(port, () => {
   connectDB();
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
