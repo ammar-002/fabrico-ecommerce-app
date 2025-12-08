1af60a344fa62eb552927bebeba5fbc76d10f7d2
Muhammad Ammar <iammuhammadammar2@gmail.com>
2025-12-09 01:32:42 +0500

    update API endpoints to use production URLs

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
-export const PRODUCTS_API_END_POINT = "http://localhost:8000/api/v1/product";
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const CART_API_END_POINT = "http://localhost:8000/api/v1/cart";
-export const ORDER_API_END_POINT = "http://localhost:8000/api/v1/order"
+export const PRODUCTS_API_END_POINT = "https://fabrico-ecommerce-app.vercel.app//api/v1/product";
+export const USER_API_END_POINT = "https://fabrico-ecommerce-app.vercel.app//api/v1/user";
+export const CART_API_END_POINT = "https://fabrico-ecommerce-app.vercel.app//api/v1/cart";
+export const ORDER_API_END_POINT = "https://fabrico-ecommerce-app.vercel.app//api/v1/order"
